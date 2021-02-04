57b8ff0f15b34ebb5b05c2d448ac877d611e4566
Roel Schellens <roels@Microsoft.com>
2021-02-04 19:23:44 +1100

    .

---
+++ solutions/14_Building_Images_WVD/AIB Build.ps1
@@ -0,0 +1,24 @@
+# destination AIB RG
+$imageResourceGroup="AIB-RG"
+
+# location (see possible locations in main docs)
+$location="westus2"
+
+# Image template submission
+$subscriptionID="74eaa7c3-20cd-428c-90c4-24cee8f0be25"
+Set-AzContext -Subscription $subscriptionID
+$imageTemplateName="wvd10ImageTemplate01"
+$templateFilePath = "armTemplateWVD.json"
+
+New-AzResourceGroupDeployment -ResourceGroupName $imageResourceGroup -TemplateFile $templateFilePath -api-version "2020-02-14" -imageTemplateName $imageTemplateName -svclocation $location
+
+# Start build
+Start-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName -NoWait
+
+# Check Build Status
+$getStatus=$(Get-AzImageBuilderTemplate -ResourceGroupName $imageResourceGroup -Name $imageTemplateName)
+$getStatus | Format-List -Property *
+# these show the status the build
+$getStatus.LastRunStatusRunState 
+$getStatus.LastRunStatusMessage
+$getStatus.LastRunStatusRunSubState
