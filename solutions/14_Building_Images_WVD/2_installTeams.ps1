13403c08939e40bff8e8cda1f9aec339df64383e
Roel Schellens <roels@Microsoft.com>
2021-02-05 12:57:23 +1100

    .

--- solutions/14_Building_Images_WVD/2_installTeams.ps1
+++ solutions/14_Building_Images_WVD/2_installTeams.ps1
@@ -39,32 +39,3 @@
  Start-Process -FilePath msiexec.exe -Args "/I $outputPath /quiet /norestart /log teams.log ALLUSER=1 ALLUSERS=1" -Wait
  write-host 'AIB Customization: Finished Install MS Teams' 
 
-
- write-host 'AIB Customization: Creates firewall rules for Teams.'
- <#
-.SYNOPSIS
-   Creates firewall rules for Teams.
-.DESCRIPTION
-   (c) Microsoft Corporation 2018. All rights reserved. Script provided as-is without any warranty of any kind. Use it freely at your own risks.
-   Must be run with elevated permissions. Can be run as a GPO Computer Startup script, or as a Scheduled Task with elevated permissions. 
-   The script will create a new inbound firewall rule for each user folder found in c:\users. 
-   Requires PowerShell 3.0.
-#>
-
-#Requires -Version 3
-
-$users = Get-ChildItem (Join-Path -Path $env:SystemDrive -ChildPath 'Users') -Exclude 'Public', 'ADMINI~*'
-if ($null -ne $users) {
-    foreach ($user in $users) {
-        $progPath = Join-Path -Path $user.FullName -ChildPath "AppData\Local\Microsoft\Teams\Current\Teams.exe"
-        if (Test-Path $progPath) {
-            if (-not (Get-NetFirewallApplicationFilter -Program $progPath -ErrorAction SilentlyContinue)) {
-                $ruleName = "Teams.exe for user $($user.Name)"
-                "UDP", "TCP" | ForEach-Object { New-NetFirewallRule -DisplayName $ruleName -Direction Inbound -Profile Domain -Program $progPath -Action Allow -Protocol $_ }
-                Clear-Variable ruleName
-            }
-        }
-        Clear-Variable progPath
-    }
-}
-write-host 'AIB Customization: Creates firewall rules for Teams. Finished' 
